ac8f6daf361c46b7633dcc5997ff0bde9fee25d9
Ilia Sadykov <smecsia@gmail.com>
2024-04-10 20:25:18 +0300

    aws policies for ecs_fargate refine

--- pkg/clouds/pulumi/aws/ecs_fargate.go
+++ pkg/clouds/pulumi/aws/ecs_fargate.go
@@ -312,10 +312,9 @@ func createEcsFargateCluster(ctx *sdk.Context, stack api.Stack, params pApi.Prov
 	params.Log.Info(ctx.Context(), "creating Fargate service for %q in %q with ingress container %q...",
 		stack.Name, deployParams.Environment, iContainer.Name)
 	service, err := ecs.NewFargateService(ctx, fmt.Sprintf("%s-service", ecsClusterName), &ecs.FargateServiceArgs{
-		Cluster:            cluster.Arn,
-		Name:               sdk.String(awsResName(ecsClusterName, "svc")),
-		DesiredCount:       sdk.Int(lo.If(crInput.Scale.Min == 0, 1).Else(crInput.Scale.Min)),
-		ForceNewDeployment: sdk.BoolPtr(true),
+		Cluster:      cluster.Arn,
+		Name:         sdk.String(awsResName(ecsClusterName, "svc")),
+		DesiredCount: sdk.Int(lo.If(crInput.Scale.Min == 0, 1).Else(crInput.Scale.Min)),
 		TaskDefinitionArgs: &ecs.FargateServiceTaskDefinitionArgs{
 			Family:     sdk.String(fmt.Sprintf("%s-%s", stack.Name, deployParams.Environment)),
 			Cpu:        sdk.String(lo.If(crInput.Config.Cpu == 0, "256").Else(strconv.Itoa(crInput.Config.Cpu))),
