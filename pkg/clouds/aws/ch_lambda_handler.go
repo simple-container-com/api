45a797e340ede59b17b8200d804dc2fc8b033322
Ilia Sadykov <smecsia@gmail.com>
2024-05-13 19:44:59 +0300

    log event from cloudwatch

--- pkg/clouds/aws/ch_lambda_handler.go
+++ pkg/clouds/aws/ch_lambda_handler.go
@@ -2,9 +2,7 @@ package aws
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net/url"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,25 +12,9 @@ import (
 
 const CloudHelperLambda = "sc-helper-aws-lambda"
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Parse form data from the request body
-	parsedFormData, err := url.ParseQuery(request.Body)
-	if err != nil {
-		log.Printf("Error parsing form data: %s", err)
-		return events.APIGatewayProxyResponse{StatusCode: 400}, err
-	}
-
-	// Access POST parameters
-	key1 := parsedFormData.Get("key1")
-	key2 := parsedFormData.Get("key2")
-
-	log.Printf("Received Key1: %s", key1)
-	log.Printf("Received Key2: %s", key2)
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       fmt.Sprintf(`%s %s`, key1, key2),
-	}, nil
+func handler(ctx context.Context, event events.CloudWatchEvent) error {
+	log.Printf("Received event: %v", event)
+	return nil
 }
 
 type lambdaCloudHelper struct {
