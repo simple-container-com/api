58cd7992381b7d78cbaaa56a3a6787362cb4431e
Vitaly Nikitin <Vit.Nikitin@gmail.com>
2025-11-12 23:32:21 +0300

    Feature/implement gcp bucket (#36)

--- pkg/clouds/pulumi/gcp/bucket.go
+++ pkg/clouds/pulumi/gcp/bucket.go
@@ -1,14 +1,19 @@
 package gcp
 
 import (
-	"github.com/pkg/errors"
+	"fmt"
 
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
+
+	"github.com/pulumi/pulumi-gcp/sdk/v8/go/gcp/serviceaccount"
 	"github.com/pulumi/pulumi-gcp/sdk/v8/go/gcp/storage"
 	sdk "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 
 	"github.com/simple-container-com/api/pkg/api"
 	"github.com/simple-container-com/api/pkg/clouds/gcloud"
 	pApi "github.com/simple-container-com/api/pkg/clouds/pulumi/api"
+	"github.com/simple-container-com/api/pkg/util"
 )
 
 func PrivateBucket(ctx *sdk.Context, stack api.Stack, input api.ResourceInput, params pApi.ProvisionParams) (*api.ResourceOutput, error) {
@@ -21,23 +26,209 @@ func PrivateBucket(ctx *sdk.Context, stack api.Stack, input api.ResourceInput, p
 		return nil, errors.Errorf("failed to convert bucket config for %q", input.Descriptor.Type)
 	}
 
-	bucket, err := storage.NewBucket(ctx, input.ToResName(bucketCfg.Name), &storage.BucketArgs{
-		Name:     sdk.String(input.ToResName(bucketCfg.Name)),
+	bucketName := input.ToResName(bucketCfg.Name)
+	opts := []sdk.ResourceOption{sdk.Provider(params.Provider)}
+
+	params.Log.Info(ctx.Context(), "creating GCS bucket %q with S3 interoperability", bucketName)
+
+	// Create the bucket
+	bucket, err := storage.NewBucket(ctx, bucketName, &storage.BucketArgs{
+		Name:     sdk.String(bucketName),
 		Location: sdk.String(bucketCfg.Location),
-	}, sdk.Provider(params.Provider))
+	}, opts...)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to provision bucket %q", bucketCfg.Name)
+		return nil, errors.Wrapf(err, "failed to provision bucket %q", bucketName)
 	}
+
+	// Export bucket name and location
+	ctx.Export(toBucketNameExport(bucketName), bucket.Name)
+	ctx.Export(toBucketLocationExport(bucketName), bucket.Location)
+
+	params.Log.Info(ctx.Context(), "creating service account for bucket %q", bucketName)
+
+	// Create a service account for S3-compatible access
+	sa, err := serviceaccount.NewAccount(ctx, fmt.Sprintf("%s-sa", bucketName), &serviceaccount.AccountArgs{
+		AccountId:   sdk.String(fmt.Sprintf("%s-sa", bucketName)),
+		DisplayName: sdk.String(fmt.Sprintf("Service Account for %s", bucketName)),
+	}, opts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create service account for bucket %q", bucketName)
+	}
+
+	params.Log.Info(ctx.Context(), "granting storage.objectAdmin role to service account for bucket %q", bucketName)
+
+	// Grant storage.objectAdmin role to the service account on the bucket
+	_, err = storage.NewBucketIAMMember(ctx, fmt.Sprintf("%s-iam", bucketName), &storage.BucketIAMMemberArgs{
+		Bucket: bucket.Name,
+		Role:   sdk.String("roles/storage.objectAdmin"),
+		Member: sa.Email.ApplyT(func(email string) string {
+			return fmt.Sprintf("serviceAccount:%s", email)
+		}).(sdk.StringOutput),
+	}, opts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to grant permissions to service account for bucket %q", bucketName)
+	}
+
+	params.Log.Info(ctx.Context(), "generating HMAC keys for S3-compatible access to bucket %q", bucketName)
+
+	// Generate HMAC keys for S3-compatible access
+	hmacKey, err := storage.NewHmacKey(ctx, fmt.Sprintf("%s-hmac", bucketName), &storage.HmacKeyArgs{
+		ServiceAccountEmail: sa.Email,
+	}, opts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create HMAC key for bucket %q", bucketName)
+	}
+
+	// Export HMAC credentials (Access Key ID and Secret Key)
+	ctx.Export(toBucketHmacAccessKeyIdExport(bucketName), hmacKey.AccessId)
+	ctx.Export(toBucketHmacSecretKeyExport(bucketName), sdk.ToSecret(hmacKey.Secret))
+
+	params.Log.Info(ctx.Context(), "successfully provisioned GCS bucket %q with S3 interoperability", bucketName)
 
 	return &api.ResourceOutput{Ref: bucket}, nil
 }
 
 func BucketComputeProcessor(ctx *sdk.Context, stack api.Stack, input api.ResourceInput, collector pApi.ComputeContextCollector, params pApi.ProvisionParams) (*api.ResourceOutput, error) {
-	params.Log.Error(ctx.Context(), "not implemented for gcp bucket")
+	if params.ParentStack == nil {
+		return nil, errors.Errorf("parent stack must not be nil for compute processor for %q", stack.Name)
+	}
+	parentStackName := params.ParentStack.StackName
 
-	// aiwayz private TODO
+	bucketCfg, ok := input.Descriptor.Config.Config.(*gcloud.GcpBucket)
+	if !ok {
+		return nil, errors.Errorf("failed to convert bucket config for %q", input.Descriptor.Type)
+	}
+
+	bucketName := input.ToResName(lo.If(bucketCfg.Name == "", input.Descriptor.Name).Else(bucketCfg.Name))
+
+	// Create a StackReference to the parent stack
+	suffix := lo.If(params.ParentStack.DependsOnResource != nil, "--"+lo.FromPtr(params.ParentStack.DependsOnResource).Name).Else("")
+	params.Log.Info(ctx.Context(), "getting parent's (%q) outputs for gcp bucket %q (%q)", params.ParentStack.FullReference, bucketName, suffix)
+	parentRef, err := sdk.NewStackReference(ctx, fmt.Sprintf("%s--%s--%s%s--gcs-bucket-ref", stack.Name, params.ParentStack.StackName, input.Descriptor.Name, suffix), &sdk.StackReferenceArgs{
+		Name: sdk.String(params.ParentStack.FullReference).ToStringOutput(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Get bucket name from parent stack
+	bucketNameExport := toBucketNameExport(bucketName)
+	resBucketName, err := pApi.GetParentOutput(parentRef, bucketNameExport, params.ParentStack.FullReference, false)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get bucket name from parent stack for %q (%q)", stack.Name, bucketNameExport)
+	} else if resBucketName == "" {
+		return nil, errors.Errorf("bucket name is empty for %q (%q)", stack.Name, bucketNameExport)
+	}
+
+	// Get HMAC access key ID from parent stack
+	accessKeyIdExport := toBucketHmacAccessKeyIdExport(bucketName)
+	resAccessKeyId, err := pApi.GetParentOutput(parentRef, accessKeyIdExport, params.ParentStack.FullReference, false)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get HMAC access key ID from parent stack for %q (%q)", stack.Name, accessKeyIdExport)
+	} else if resAccessKeyId == "" {
+		return nil, errors.Errorf("HMAC access key ID is empty for %q (%q)", stack.Name, accessKeyIdExport)
+	}
+
+	// Get HMAC secret key from parent stack
+	secretKeyExport := toBucketHmacSecretKeyExport(bucketName)
+	resSecretKey, err := pApi.GetParentOutput(parentRef, secretKeyExport, params.ParentStack.FullReference, true)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get HMAC secret key from parent stack for %q (%q)", stack.Name, secretKeyExport)
+	} else if resSecretKey == "" {
+		return nil, errors.Errorf("HMAC secret key is empty for %q (%q)", stack.Name, secretKeyExport)
+	}
+
+	// Get bucket location from parent stack
+	locationExport := toBucketLocationExport(bucketName)
+	resBucketLocation, err := pApi.GetParentOutput(parentRef, locationExport, params.ParentStack.FullReference, false)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get bucket location from parent stack for %q (%q)", stack.Name, locationExport)
+	} else if resBucketLocation == "" {
+		return nil, errors.Errorf("bucket location is empty for %q (%q)", stack.Name, locationExport)
+	}
+
+	collector.AddOutput(ctx, parentRef.Name.ApplyT(func(refName any) any {
+		// S3-compatible endpoint for GCS
+		s3Endpoint := "https://storage.googleapis.com"
+
+		// Add bucket-specific environment variables
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("GCS_%s_BUCKET", bucketName)), resBucketName,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("GCS_%s_LOCATION", bucketName)), resBucketLocation,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("GCS_%s_ACCESS_KEY", bucketName)), resAccessKeyId,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("GCS_%s_SECRET_KEY", bucketName)), resSecretKey,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("GCS_%s_ENDPOINT", bucketName)), s3Endpoint,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+
+		// Add S3-compatible environment variables for applications expecting S3
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("S3_%s_BUCKET", bucketName)), resBucketName,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("S3_%s_REGION", bucketName)), resBucketLocation,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("S3_%s_ACCESS_KEY", bucketName)), resAccessKeyId,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("S3_%s_SECRET_KEY", bucketName)), resSecretKey,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName(fmt.Sprintf("S3_%s_ENDPOINT", bucketName)), s3Endpoint,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+
+		// Add generic environment variables (without bucket name prefix)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("GCS_BUCKET"), resBucketName,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("GCS_LOCATION"), resBucketLocation,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName("GCS_ACCESS_KEY"), resAccessKeyId,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName("GCS_SECRET_KEY"), resSecretKey,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("GCS_ENDPOINT"), s3Endpoint,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+
+		// Add AWS SDK compatible environment variables
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName("AWS_ACCESS_KEY_ID"), resAccessKeyId,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddSecretEnvVariableIfNotExist(util.ToEnvVariableName("AWS_SECRET_ACCESS_KEY"), resSecretKey,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("S3_ENDPOINT"), s3Endpoint,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("S3_BUCKET"), resBucketName,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+		collector.AddEnvVariableIfNotExist(util.ToEnvVariableName("S3_REGION"), resBucketLocation,
+			input.Descriptor.Type, input.Descriptor.Name, parentStackName)
+
+		// Add resource template extension for programmatic access
+		collector.AddResourceTplExtension(input.Descriptor.Name, map[string]string{
+			"bucket":     resBucketName,
+			"location":   resBucketLocation,
+			"access-key": resAccessKeyId,
+			"secret-key": resSecretKey,
+			"endpoint":   s3Endpoint,
+		})
+
+		return nil
+	}))
 
 	return &api.ResourceOutput{
-		Ref: nil,
+		Ref: parentStackName,
 	}, nil
+}
+
+// Helper functions for export names
+func toBucketNameExport(bucketName string) string {
+	return fmt.Sprintf("%s-bucket-name", bucketName)
+}
+
+func toBucketLocationExport(bucketName string) string {
+	return fmt.Sprintf("%s-bucket-location", bucketName)
+}
+
+func toBucketHmacAccessKeyIdExport(bucketName string) string {
+	return fmt.Sprintf("%s-hmac-access-key-id", bucketName)
+}
+
+func toBucketHmacSecretKeyExport(bucketName string) string {
+	return fmt.Sprintf("%s-hmac-secret-key", bucketName)
 }
